f4e0dbda1cbebaa5882d44e422111a421081336b
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2019-01-04 22:04:13 +0100

    Initial commit

---
+++ app/src/main/java/com/aouatefandroid/splashscreen/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.aouatefandroid.splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this, Welcome.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
